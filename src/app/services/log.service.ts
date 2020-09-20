b1d1cfb5485e14765002a7865e080d8eb1a5dc41
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-09-20 01:09:58 +0100

    [LogService] Work with localStorage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
@@ -10,41 +11,49 @@ import { Log } from '../models/log';
 export class LogService {
 
   // Attributes
-  logs: Log[];
+  devmateData: any;
 
   // Constructor
-  constructor() { 
-    this.logs = [
-      {id:2, title: 'Set Routes', updatedAt: new Date("1/3/2020")},
-      {id:1, title: 'Add Services', updatedAt: new Date("1/2/2020")},
-      {id:0, title: 'Create log component', updatedAt: new Date("2020-09-12")},
-    ];
+  constructor() {
+    if(localStorage.getItem('devmate') == null){
+      this.devmateData = {
+        lastId: -1,
+        logs: new Array
+      };
+      localStorage.setItem('devmate', JSON.stringify(this.devmateData));
+    } else {
+      this.devmateData = JSON.parse(localStorage.getItem('devmate'));
+    }
   }
 
   // GET all logs
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    return of(this.devmateData.logs);
   }
 
   // POST a log
   addLog(log: Log): void {
-    log.id = this.logs.length == 0 ? 0 : this.logs[0].id + 1;
-    this.logs.unshift(log);
-    console.log(this.logs);
+    log.id = ++this.devmateData.lastId;
+    this.devmateData.logs.unshift(log);
+    localStorage.setItem('devmate', JSON.stringify(this.devmateData));
   }
 
   // DELETE a log
   deleteLog(logToDelete: Log): void {
-    this.logs.forEach((log, index) => {
-      if(logToDelete.id === log.id) this.logs.splice(index, 1);
+    this.devmateData.logs.forEach((log, index) => {
+      if(logToDelete.id === log.id) this.devmateData.logs.splice(index, 1);
     });
+    localStorage.setItem('devmate', JSON.stringify(this.devmateData));
   }
 
   // PUT a log
   updateLog(editedLog: Log): Observable<Log>{
     editedLog.updatedAt = new Date();
-    this.deleteLog(editedLog);
-    this.logs.unshift(editedLog);
+    this.devmateData.logs.forEach((log, index) => {
+      if(editedLog.id === log.id) this.devmateData.logs.splice(index, 1);
+    });
+    this.devmateData.logs.unshift(editedLog);
+    localStorage.setItem('devmate', JSON.stringify(this.devmateData));
 
     return of(editedLog);
   }
